e1f2eacb20364f4d78f038ae3b9111ad802c9d69
Tales Medeiros <t4les313@gmail.com>
2018-09-09 23:07:17 -0300

    #

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/statics/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
@@ -140,5 +140,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #]
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'statics'),
+    os.path.join(BASE_DIR, 'static'),
 )
